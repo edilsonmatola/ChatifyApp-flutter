7c1b4a88eeb4779678aa752cada51dc94fad4cb4
Edilson Matola <edilsonmatola13@gmail.com>
2022-01-20 22:46:06 +0200

    Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 // Packages
 import 'package:flutter/material.dart';
 
+// Pages
+import '../pages/chats_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,12 +12,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Page index
+  int _currentPage = 0;
+  // * Pages to display and navigate
+  final List<Widget> _pages = [
+    const ChatsPage(),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'CHATS',
+            icon: Icon(
+              Icons.chat_bubble_sharp,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'USERS',
+            icon: Icon(
+              Icons.supervised_user_circle,
+            ),
+          ),
+        ],
+        onTap: (_index) {
+          setState(
+            () {
+              _currentPage = _index;
+            },
+          );
+        },
+      ),
+    );
   }
 }
