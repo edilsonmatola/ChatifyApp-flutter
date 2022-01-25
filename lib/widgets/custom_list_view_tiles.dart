06d3a1247d2393fc64f7e1b9e683e336e3fee230
Edilson Matola <edilsonmatola13@gmail.com>
2022-01-25 22:37:23 +0200

    chore: Created tile for user chats

---
+++ lib/widgets/custom_list_view_tiles.dart
@@ -0,0 +1,74 @@
+// Packages
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart'; //Custom Animations
+
+// Widgets
+import '../widgets/rounded_image_network.dart';
+
+// Models
+import '../models/chat_message_model.dart';
+import '../models/chat_user_model.dart';
+
+class CustomListViewTileWithActivity extends StatelessWidget {
+  const CustomListViewTileWithActivity({
+    Key? key,
+    required this.height,
+    required this.title,
+    required this.subtitle,
+    required this.imagePath,
+    required this.isActive,
+    required this.isActivity,
+    required this.onTap,
+  }) : super(key: key);
+
+  final double height;
+  final String title;
+  final String subtitle;
+  final String imagePath;
+  final bool isActive;
+  final bool isActivity;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      minVerticalPadding: height * .20,
+      onTap: () => onTap(),
+      leading: RoundedIMageNetworkWithStatusIndicator(
+        key: UniqueKey(),
+        imagePath: imagePath,
+        size: height / 2,
+        isActive: isActive,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      subtitle: isActivity
+          ? Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // * Typing animation (...)
+                SpinKitThreeBounce(
+                  color: Colors.white54,
+                  size: height * .10,
+                ),
+              ],
+            )
+          : Text(
+              subtitle,
+              style: const TextStyle(
+                color: Colors.white54,
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+    );
+  }
+}
