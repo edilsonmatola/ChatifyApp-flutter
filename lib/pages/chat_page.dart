9d801df1257555dac599a1a97dc142c9dc2344e5
Edilson Matola <edilsonmatola13@gmail.com>
2022-02-04 14:50:59 +0200

    chore: Added Top Bar

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -34,10 +34,47 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _deviceHeight = MediaQuery.of(context).size.height;
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: _deviceWidth * .03, vertical: _deviceHeight * .02),
+          width: _deviceWidth,
+          height: _deviceHeight,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TopBar(
+                widget.chat.title(),
+                fontSize: 16,
+                primaryAction: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Color.fromRGBO(0, 82, 218, 1),
+                  ),
+                ),
+                secondaryAction: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Color.fromRGBO(0, 82, 218, 1),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
