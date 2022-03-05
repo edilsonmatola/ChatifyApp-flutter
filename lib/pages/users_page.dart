cd6243e6e1646ab4dea5785e2fce1679031f84a0
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-05 21:50:28 +0200

    Added Topbar Menu of the Users Page

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,7 +1,18 @@
 // Packages
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 // Providers
+import '../providers/authentication_provider.dart';
+
+// Widgets
+import '../widgets/top_bar.dart';
+import '../widgets/custom_input_fields.dart';
+import '../widgets/custom_list_view_tiles.dart';
+import '../widgets/rounded_button.dart';
+
+// Models
+import '../models/chat_user_model.dart';
 
 class UsersPage extends StatefulWidget {
   const UsersPage({Key? key}) : super(key: key);
@@ -11,10 +22,50 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
+  late double _deviceWidth;
+  late double _deviceHeight;
+
+  late AuthenticationProvider _auth;
+
+  final TextEditingController _searchFieldTextEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _auth = Provider.of<AuthenticationProvider>(context);
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Container(
+      width: _deviceWidth * .97,
+      height: _deviceHeight * .98,
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * .03,
+        vertical: _deviceHeight * .02,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          TopBar(
+            'Users',
+            primaryAction: IconButton(
+              onPressed: () {
+                // * Logout the user if he/she presses the button icon
+                _auth.logout();
+              },
+              icon: const Icon(
+                Icons.logout_outlined,
+                color: Color.fromRGBO(0, 82, 218, 1),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
