65ca2a9280748f8f15bae5dd716030738e535534
Edilson Matola <edilsonmatola13@gmail.com>
2022-09-13 14:28:44 +0200

    Home Screen

---
+++ lib/src/features/home/presentation/home_screen/home_screen.dart
@@ -0,0 +1,54 @@
+// Packages
+import 'package:flutter/material.dart';
+
+// Pages
+import '../../../contacts/contacts_export.dart';
+import '../../../groups/presentation/group_screen/chats_page.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // Page index
+  int _currentPage = 0;
+  // * Pages to display and navigate
+  final List<Widget> _pages = [
+    const ChatsPage(),
+    const UsersPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'CHATS',
+            icon: Icon(
+              Icons.chat_bubble_sharp,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'USERS',
+            icon: Icon(
+              Icons.supervised_user_circle,
+            ),
+          ),
+        ],
+        onTap: (_index) {
+          setState(
+            () {
+              _currentPage = _index;
+            },
+          );
+        },
+      ),
+    );
+  }
+}
